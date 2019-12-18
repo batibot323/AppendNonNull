e8b2d68f2bbf2c2a0951553c389fed82d8fb439a
batibot323 <hani.herbert.ho@gmail.com>
2019-12-18 23:19:12 +0800

    Initial commit.

---
+++ Ho.Scratch.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ho.Scratch.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string addressOne = "myOne";
+            string addressTwo = "myTwo";
+            string addressThree = "myThree";
+            string addressFour = "myFour";
+            string fullAddress = AppendNonNullStrings(addressOne, addressTwo, addressThree, addressFour);
+            Console.WriteLine(fullAddress);
+            addressTwo = null;
+            addressThree = null;
+            fullAddress = AppendNonNullStrings(addressOne, addressTwo, addressThree, addressFour);
+            Console.WriteLine(fullAddress);
+        }
+
+        private static string AppendNonNullStrings(params string[] strings)
+        {
+            string fullString = "";
+            foreach (string input in strings)
+            {
+                fullString += (String.IsNullOrEmpty(input) ? "" : $"{ input } ");
+            }
+            return fullString;
+        }
+    }
+}
